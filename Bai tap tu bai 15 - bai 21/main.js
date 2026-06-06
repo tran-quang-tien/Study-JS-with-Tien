97d37aa44d7d69fd3167035072658db836acd089
tran-quang-tien <shidou209@gmail.com>
2026-06-06 23:33:15 +0700

    day 4

---
+++ Bai tap tu bai 15 - bai 21/main.js
@@ -0,0 +1,42 @@
+// Bài 15
+// Cách 1
+let n = Number(prompt("nhap vao so n"));
+let giaithua = 1;
+for( let i = 1;i<=n;i++){
+    giaithua*=i;
+}
+console.log(giaithua);
+//Cách 2
+let m = prompt("nhap vao so n");
+let giaithua2 = 1;
+let i = 1;
+while(i<=m){
+    giaithua2*=i;
+    i++;
+}
+console.log(giaithua2);
+//Bài 16
+let a = prompt("nhap vao so a");
+let tong = 0
+if(a%2==0){
+    for(let i = 0;i<=a;i+=2){
+        tong+=i;
+    }
+    
+
+}else{
+    console.log(`toi k tinh so le`)
+}
+console.log(tong);
+//Bài 17
+let b = prompt("nhap vao so b");
+let tongle =0;
+if(b%2!==0){
+    for(let i=1;i<=b;i+=2){
+        if(i=3){
+            continue;
+        }else{
+        tongle+=1}
+    }
+}
+console.log(tongle);
